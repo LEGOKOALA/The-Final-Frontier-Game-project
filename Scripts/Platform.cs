459541d3f3e25dbe456ea4299672cf3c3eee6f72
MI7xZY <144147004+LEGOKOALA@users.noreply.github.com>
2025-09-17 12:12:56 -0400

    moving Platform finalized

--- Scripts/Platform.cs
+++ Scripts/Platform.cs
@@ -3,11 +3,12 @@ using System;
 
 public partial class Platform : AnimatableBody2D
 {
-	sprite=Node.GetChild ("Sprite2D")
-	sprite.FlipH = True
 	
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{ 
+	public void FlipSprite() {
+		var sprite = GetChild<Sprite2D>(0);
+		var exhaust = GetChild<AnimatedSprite2D>(2);
+		sprite.FlipH = !sprite.FlipH;
+		exhaust.FlipH = !exhaust.FlipH;
+		exhaust.Position *= new Vector2(-1, 1);
 	}
 }
