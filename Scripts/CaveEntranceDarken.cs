aaab2362624a8d9129af619a9145a0b9129d4a71
Cbailey10! <crcbailey10@gmail.com>
2025-09-22 21:00:22 -0400

    IT ALL WORKSSSS!!!!!

--- Scripts/CaveEntranceDarken.cs
+++ Scripts/CaveEntranceDarken.cs
@@ -1,26 +1,34 @@
 using Godot;
 using System;
 
-
 public partial class CaveEntranceDarken : Area2D
 {
+	private ColorRect colorrect;
+	private ColorRect colorrect2;
+
+	public override void _Ready()
+	{
+		colorrect = GetNode<ColorRect>("ColorRect");
+		colorrect2 = GetNode<ColorRect>("ColorRect2");
+		Connect("body_entered", new Callable(this, nameof(OnBodyEntered)));
+		Connect("body_exited", new Callable(this, nameof(OnBodyExited)));
+	}
+
 	private void OnBodyEntered(Node2D body)
 	{
 		if (body is Player)
 		{
-			GetNode<ColorRect>("ColorRect").Visible = true;
+			colorrect.Visible = true;
+			colorrect2.Visible = false;
 		}
 	}
-
-
-
-
 
 	private void OnBodyExited(Node2D body)
 	{
 		if (body is Player)
 		{
-			GetNode<ColorRect>("ColorRect").Visible = false;
+			colorrect.Visible = false;
+			colorrect2.Visible = true;
 		}
 	}
 }
