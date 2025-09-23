f4243035db4964819edb78dfae0ee23dddda8525
Cbailey10! <crcbailey10@gmail.com>
2025-09-23 11:04:30 -0400

    yay

--- Scripts/CaveEntranceDarken.cs
+++ Scripts/CaveEntranceDarken.cs
@@ -5,11 +5,14 @@ public partial class CaveEntranceDarken : Area2D
 {
 	private ColorRect colorrect;
 	private ColorRect colorrect2;
+	private ColorRect colorrect3;
+
 
 	public override void _Ready()
 	{
 		colorrect = GetNode<ColorRect>("ColorRect");
 		colorrect2 = GetNode<ColorRect>("ColorRect2");
+		colorrect3 = GetNode<ColorRect>("ColorRect3");
 		Connect("body_entered", new Callable(this, nameof(OnBodyEntered)));
 		Connect("body_exited", new Callable(this, nameof(OnBodyExited)));
 	}
@@ -20,6 +23,7 @@ public partial class CaveEntranceDarken : Area2D
 		{
 			colorrect.Visible = true;
 			colorrect2.Visible = false;
+			colorrect3.Visible = false;
 		}
 	}
 
@@ -29,6 +33,7 @@ public partial class CaveEntranceDarken : Area2D
 		{
 			colorrect.Visible = false;
 			colorrect2.Visible = true;
+			colorrect3.Visible = true;
 		}
 	}
 }
