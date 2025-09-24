8637306b7cc5780355bf31b8750346cab75c9cc2
Cbailey10! <crcbailey10@gmail.com>
2025-09-24 11:00:55 -0400

    idk

--- Scripts/Astroid.cs
+++ Scripts/Astroid.cs
@@ -2,13 +2,13 @@ using Godot;
 using System;
 using System.Collections.Generic;
 
-public partial class asteroid; Asteroid
-public override void _PhysicsProcess(double delta)
-{
-	Vector2 velocity = Velocity;
-	velocity.X = (float)(player_x * speed * delta);
-	velocity.Y = (float)(player_y * speed * delta);
-	Velocity = velocity;
-	MoveAndSlide;
+public partial class CharacterBody2D; Asteroid
+	public override void _PhysicsProcess(double delta)
+	{
+		Vector2 velocity = Velocity;
+		velocity.X = (float)(player_x * speed * delta);
+		velocity.Y = (float)(player_y * speed * delta);
+		Velocity = velocity;
+		MoveAndSlide;
 	
-}
+	}
