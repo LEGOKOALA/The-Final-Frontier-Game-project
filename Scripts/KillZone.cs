485a50c0b0d03213e7368a9d6ff0af711b9d6d59
Cbailey10! <crcbailey10@gmail.com>
2025-09-23 20:15:43 -0400

    Revert "Merge branch 'asteroid'"
    This reverts commit 3a830496cf47da156d56a24147b8d2de17f7db9c, reversing
    changes made to 500e2b039e9a61180f1ed56d5c9bb3c482e825bf.

--- Scripts/KillZone.cs
+++ Scripts/KillZone.cs
@@ -3,15 +3,21 @@ using System;
 
 public partial class KillZone : Area2D 
 {
+	// The signal automatically connects to this method.
 	private void OnBodyEntered(Node2D body)
 	{
+		// Check if the colliding body is the player.
+		// The `is` keyword is a clean way to check the type and cast it.
 		if (body is Player)
 		{
+			// Call the player's death method.
+			// You will need to implement a "Die()" method in your Player.cs script.
 			((Player)body).Die();
 			GD.Print("Player entered kill zone!");
-			
+			// Get the current scene.
 			var currentScene = GetTree().CurrentScene;
 		
+			// Reload the current scene.
 			GetTree().ReloadCurrentScene();
 
 			GD.Print("Player died and scene reset.");
