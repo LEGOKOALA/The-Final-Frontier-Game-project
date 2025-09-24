24aa3de8bf05f30d53b25da5dd912c23100cbfc7
Cbailey10! <crcbailey10@gmail.com>
2025-09-23 21:02:11 -0400

    Fixing gravity and in-air movement speed

--- Scripts/Player.cs
+++ Scripts/Player.cs
@@ -4,45 +4,33 @@ using System;
 public partial class Player : CharacterBody2D
 {
 	public const float Speed = 130.0f;
-	public const float JumpVelocity = -200.0f;
+	public const float JumpVelocity = -60.0f;
+	public const float Gravity = 60.0f;
 
 	[Signal]
 	public delegate void DiedEventHandler();
 
 	public void Die()
 	{
-		// Emit a signal for other parts of the game (like a UI) to know the player died.
 		EmitSignal(SignalName.Died);
-
-		// For this example, we simply hide the player and disable collision.
-		// More advanced games might use a respawn system, an animation, or destroy the node.
-		QueueFree(); // Deletes the player node from the scene tree.
-
-		// OR:
-		// SetProcessMode(ProcessModeEnum.Disabled); // Stops processing for the player.
-		// Hide(); // Hides the player mesh or sprite.
-		// GetNode<CollisionShape2D>("CollisionShape2D").Disabled = true;
+		QueueFree();
 	}
+
 	public override void _PhysicsProcess(double delta)
 	{
 		Vector2 velocity = Velocity;
 
-
-		// Add the gravity.
 		if (!IsOnFloor())
 		{
-			velocity += GetGravity() * (float)delta;
+			velocity.Y += Gravity * (float)delta;
 		}
 
-		// Handle Jump.
 		if (Input.IsActionJustPressed("ui_up") && IsOnFloor())
 		{
 			velocity.Y = JumpVelocity;
 			
 		}
 
-		// Get the input direction and handle the movement/deceleration.
-		// As good practice, you should replace UI actions with custom gameplay actions.
 		Vector2 direction = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
 		if (direction != Vector2.Zero)
 		{
@@ -63,8 +51,5 @@ public partial class Player : CharacterBody2D
 
 		Velocity = velocity;
 		MoveAndSlide();
-
-
-
 	}
 }
