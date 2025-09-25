6ddefbc6d0dbb9f3c78878e827c460abc40aaa2a
Cbailey10! <crcbailey10@gmail.com>
2025-09-25 10:51:35 -0400

    hmmmm

--- Scripts/Asteroid.cs
+++ Scripts/Asteroid.cs
@@ -1,26 +1,21 @@
 using Godot;
 using System;
+using System.Collections.Generic;
 
 public partial class Asteroid : CharacterBody2D
 {
-	public Vector2 Velocity = new Vector2(60, 60);
-	public float RotationSpeed = 2f;
-	private Vector2 _spawnPosition;
-
-	public override void _Ready()
-	{
-		_spawnPosition = GlobalPosition;
-	}
+	private float rotation_random = 5.0f;
+	private float speed = 1000.0f;
+	private float player_x = 1.0f;
+	private float player_y = 1.0f;
 
 	public override void _PhysicsProcess(double delta)
 	{
-		KinematicCollision2D collision = MoveAndCollide(Velocity * (float)delta);
-		Rotation += RotationSpeed * (float)delta;
-
-		if (collision != null)
-		{
-			GlobalPosition = _spawnPosition;
-			Visible = true;
-		}
+		RotationDegrees += (float)rotation_random;
+		Vector2 velocity = Velocity;
+		velocity.X = (float)(player_x * speed * delta);
+		velocity.Y = (float)(player_y * speed * delta);
+		Velocity = velocity;
+		MoveAndSlide();
 	}
 }
